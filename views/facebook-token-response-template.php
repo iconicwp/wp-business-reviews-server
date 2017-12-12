9e4359b1f8f339d02a237240b7d090e24d845d50
Kevin Hoffman <kevin@kevinwhoffman.com>
2017-12-12 04:02:07 -0500

    Add form that posts token back to WPBR settings

--- views/facebook-token-response-template.php
+++ views/facebook-token-response-template.php
@@ -1,3 +1,13 @@
-<?php get_header() ?>
-<h1>Insert Token Response Form Here</h1>
+<?php
+$token         = wpbrs_get_facebook_token();
+$wpbr_redirect = isset( $_GET['wpbr_redirect'] ) ? sanitize_text_field( $_GET['wpbr_redirect'] ): '';
+get_header();
+?>
+
+<form action="<?php echo esc_attr( $wpbr_redirect ); ?>" method="post">
+	<label for="wpbr_facebook_token"><?php esc_html_e( 'Token', 'wp-business-reviews-server' ) ?></label>
+	<input id="wpbr_facebook_token" type="text" name="wpbr_facebook_token" value="<?php echo esc_attr( $token ); ?>">
+	<input type="submit" value="<?php esc_html_e( 'Return to WP Business Reviews Settings', 'wp-business-reviews-server' ) ?>">
+</form>
+
 <?php get_footer() ?>
