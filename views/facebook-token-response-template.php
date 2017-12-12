4fcaba909241c112b2209f29aad6ffd6d4de89e4
Kevin Hoffman <kevin@kevinwhoffman.com>
2017-12-12 14:41:49 -0500

    Make token field hidden

--- views/facebook-token-response-template.php
+++ views/facebook-token-response-template.php
@@ -1,12 +1,11 @@
 <?php
-$token         = wpbrs_get_facebook_user_access_token();
-$wpbr_redirect = isset( $_GET['wpbr_redirect'] ) ? sanitize_text_field( $_GET['wpbr_redirect'] ): '';
+$user_access_token = wpbrs_get_facebook_user_access_token();
+$wpbr_redirect     = isset( $_GET['wpbr_redirect'] ) ? sanitize_text_field( $_GET['wpbr_redirect'] ): '';
 get_header();
 ?>
 
-<form action="<?php echo esc_attr( $wpbr_redirect ); ?>" method="post">
-	<label for="wpbr_facebook_token"><?php esc_html_e( 'Token', 'wp-business-reviews-server' ) ?></label>
-	<input id="wpbr_facebook_token" type="text" name="wpbr_facebook_token" value="<?php echo esc_attr( $token ); ?>">
+<form id="wpbr-facebook-user-access-token-form" action="<?php echo esc_attr( $wpbr_redirect ); ?>" method="post">
+	<input type="hidden" name="wpbr_facebook_user_access_token" value="<?php echo esc_attr( $user_access_token ); ?>">
 	<input type="submit" value="<?php esc_html_e( 'Return to WP Business Reviews Settings', 'wp-business-reviews-server' ) ?>">
 </form>
 
