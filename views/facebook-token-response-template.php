441ac2bd644bc75b45f533e215f197173bfd5da0
Devin Walker <sdtucson@gmail.com>
2018-05-08 12:54:06 -0700

    fix: add styling for redirect screen

--- views/facebook-token-response-template.php
+++ views/facebook-token-response-template.php
@@ -1,16 +1,42 @@
 <?php
 $user_access_token = wpbrs_get_facebook_user_access_token();
-$wpbr_redirect     = isset( $_GET['wpbr_redirect'] ) ? sanitize_text_field( $_GET['wpbr_redirect'] ): '';
-get_header();
+$wpbr_redirect = isset( $_GET['wpbr_redirect'] ) ? sanitize_text_field( $_GET['wpbr_redirect'] ) : '';
+wp_head();
 ?>
 
-<form id="wpbr-facebook-user-access-token-form" action="<?php echo esc_attr( $wpbr_redirect ); ?>" method="post">
-	<input type="hidden" name="wpbr_facebook_user_token" value="<?php echo esc_attr( $user_access_token ); ?>">
-	<input type="submit" value="<?php esc_html_e( 'Return to WP Business Reviews Settings', 'wp-business-reviews-server' ) ?>">
-</form>
+<style>
+	body {
+		background-color: #ECF0F1;
+	}
+	#wpbr-facebook-user-access-token-form {
+		background-color: white;
+		border-radius: 0.5rem;
+		box-shadow: 0 0.375rem 0.375rem rgba(44, 62, 80, 0.05);
+		margin: 1.5rem auto;
+		padding: 1.5rem;
+		max-width: 600px;
+		text-align:center;
+	}
+	#wpbr-facebook-user-access-token-form .button {
+		padding: 10px 20px;
+	}
+</style>
+
+<header id="masthead" class="site-header" role="banner">
+	<div class="site-header__container site-header__container--center">
+		<span class="site-logo site-logo--header"><img src="<?php echo esc_url( get_stylesheet_directory_uri() . '/assets/img/logo-wp-business-reviews.png' ); ?>"
+		                                               alt="<?php echo esc_attr__( 'WP Business Reviews', 'wpbr' ); ?>"></span>
+	</div>
+</header>
+<section class="band band--pad-v">
+	<form id="wpbr-facebook-user-access-token-form" action="<?php echo esc_attr( $wpbr_redirect ); ?>" method="post">
+		<p>Thank you for connecting to Facebook!</p>
+		<input type="hidden" name="wpbr_facebook_user_token" value="<?php echo esc_attr( $user_access_token ); ?>">
+		<input type="submit" class="button" value="<?php esc_html_e( 'Return to WP Business Reviews Settings', 'wp-business-reviews-server' ) ?>">
+	</form>
+</section>
+
 
 <script>
 	document.getElementById( 'wpbr-facebook-user-access-token-form' ).submit();
 </script>
-
-<?php get_footer() ?>
